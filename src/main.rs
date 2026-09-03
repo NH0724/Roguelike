4ff27b40b24073b81ee2b1bd77cafb38634960b4
NH0724 <huxi0820@gmail.com>
2026-09-03 01:06:41 -0400

    adding functions in player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-/*use std::string;*/
-
 struct Player {
     name: String,
     x: i32,
@@ -10,11 +8,30 @@ impl Player {
     fn new(name: String, x: i32, y: i32, hp: i32) -> Player {
         Player { name, x, y, hp }
     }
+
+    fn heal(&mut self, amount: i32) {
+        self.hp = self.hp + amount;
+    }
+
+    fn damage(&mut self, amount: i32) {
+        self.hp = self.hp - amount;
+    }
 }
+
 fn main() {
-    let player = Player::new("Tom".to_string(), 2, 3, 1000);
+    let mut player = Player::new("Tom".to_string(), 2, 3, 80);
     println!(
-        "Player {}, positiion:({},{})and HP is {}",
+        "Player {}, positiion:({},{}) and HP is {}",
         player.name, player.x, player.y, player.hp
-    )
+    );
+    player.heal(20);
+    println!(
+        "Player {}, positiion:({},{}) and HP is {}",
+        player.name, player.x, player.y, player.hp
+    );
+    player.damage(10);
+    println!(
+        "Player {}, positiion:({},{}) and HP is {}",
+        player.name, player.x, player.y, player.hp
+    );
 }
